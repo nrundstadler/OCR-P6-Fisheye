7470841aa7f34908e2dfa0cd36f718984099fd0d
Nicolas Rundstadler <n.rundstadler@laposte.net>
2024-10-18 11:48:10 +0200

    Refactor code to follow BEM convention across HTML, CSS, and JS.

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,23 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+  const { name, portrait } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    article.classList.add("photographer-card");
+
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    img.classList.add("photographer-card__img");
+
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+    h2.classList.add("photographer-card__title");
+
+    article.appendChild(img);
+    article.appendChild(h2);
+    return article;
+  }
+  return { name, picture, getUserCardDOM };
 }
